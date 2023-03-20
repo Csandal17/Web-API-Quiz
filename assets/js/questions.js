1f05dcac1c7be81a8f6792a997aa3adefc3923ec
Chantal Guiheen <chantal@Chantals-Air.fritz.box>
2023-03-20 12:54:38 +0000

    js starter code and quiz questions

---
+++ assets/js/questions.js
@@ -0,0 +1,17 @@
+let questions = [
+    {
+        "title": "HTML stands for:",
+        "choices": ["HighText Machine Language", "HyperTexts and links Markup Language", "HyperText Markup Language", "None of these"],
+        "answer": "HyperText Markup Language"
+    },
+    {
+        "title": "The correct sequence of HTML tags for starting a webpage:",
+        "choices": ["Head, Title, HTML, body", "HTML, Body, Title, Head", "HTML, Head, Title, Body", "Html"],
+        "answer": "HTML, Head, Title, Body"
+    },
+    {
+        "title": "Which of the following elements is responsible for making the text bold in HTML:",
+        "choices": ["<pre>", "<a>", "<b>", "<br>"],
+        "answer": "<b>"
+    }
+]
---
+++ assets/js/scores.js
@@ -0,0 +1,16 @@
+function printHighScores(){
+
+
+}
+
+function clearHighScores(){
+
+}
+
+let clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", clearHighScores);
+
+
+//document.getElementById("clear").onClick = clearHighScores;
+
+printHighScores()
