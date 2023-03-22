7b4cefb2878fe9098952e9af8596b04121f19f88
Chantal Guiheen <chantal@chantals-air.lan>
2023-03-22 20:36:51 +0000

    js click events, fuctions clock

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,5 @@
+const { default: Feedback } = require("react-bootstrap/esm/Feedback");
+
 let currentQuestionIndex = 0;
 let time = questions.length * 15;
 let timerID;
@@ -12,32 +14,118 @@ let feedBackElement = document.getElementById("feedback");
 
 let sfxRight = new Audio("assets/sfx/correct.wav");
 
-function getQuestion(){
-
-}
-
 function questionClick(){
+    if(this.value !== questions[currentQuestionIndex].answer) {
+        time -= 15;
 
+        if(time < 0) {
+            time = 0;
+        }
+
+        timerElement.textContent = time;
+
+        feedBackElement.textContent = "Wrong!"
+    } else {
+        sfxRight.play();
+        feedBackElement.textContent = "Correct!";
+    }
+
+
+    feedBackElement.setAttribute("class", "feedback");
+
+    setTimeout(function(){
+        feedBackElement.setAttribute("class", "feedback hide")
+    }, 1000);
+
+    currentQuestionIndex++;
+
+    if(currentQuestionIndex === questions.length) {
+        quizEnd()
+    } else {
+        getQuestion();
+    }
 }
 
-function startQuiz(){
-    alert("Hi there! Let's begin")
+function getQuestion(){
+    let currentQuestion = questions[currentQuestionIndex];
+
+    let titleElement = document.getElementById("question-title");
+
+    titleElement.textContent = currentQuestion.title;
+
+    choicesElement.innerHTML = "";
+
+    currentQuestion.choices.forEach(function(choice, index) {
+        let choiceButton = document.createElement("button");
+
+        choiceButton.setAttribute("class", "choice");
+        choiceButton.setAttribute("value", choice);
+
+        choiceButton.textContent = `${index + 1}. ${choice}`
+
+        choiceButton.addEventListener("click", questionClick);
+
+        choicesElement.append(choiceButton);
+    })
 }
 
 function quizEnd(){
+    clearInterval(timerID);
 
+    let endScreenElement = document.getElementById("end-screen");
+    endScreenElement.removeAttribute("class");
+
+    let finalScoreElement = document.getElementById("final-score");
+    finalScoreElement.textContent = time;
+
+    questionsElement.setAttribute("class", "hide");
 }
 
 function clockTick(){
+    time--;
+    timerElement.textContent = time;
 
+    if(time <= 0){
+        quizEnd();
+    }
+}
+
+function startQuiz(){
+    let startScreenElement = document.getElementById("start-screen");
+    startScreenElement.setAttribute("class", "hide");
+
+    questionsElement.removeAttribute("class");
+
+    timerID = setInterval(clockTick, 1000)
+
+    timerElement.textContent = time;
+
+    getQuestion();
 }
 
 function saveHighScore(){
+    let initials = initialElement.value.trim();
+    console.log(initials);
+
+    if(initials !== ""){
+        let highScores = JSON.parse(localStorage.getItem("highscores")) || [];
+        let newScore = {
+            score: time,
+            initials: initials
+        }
+
+        highScores.push(newScore);
+        localStorage.setItem("highscores", JSON.stringify(highScores));
+
+        window.location.href = "highscores.html";
+    }
 
 }
 
 function checkForEnter(event){
-
+    if(event.key === "Enter") {
+        saveHighScore();
+    }
 }
 
 startButton.addEventListener("click", startQuiz);
