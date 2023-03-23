0143dc0334913a87b6b59227a66ccf721c071876
Chantal Guiheen <chantal@chantals-air.lan>
2023-03-23 17:03:12 +0000

    js scores list

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,16 +1,29 @@
 function printHighScores(){
+    let highScores = JSON.parse(localStorage.getItem("highscores")) || [];
 
+    highScores.sort(function(a, b) {
+        return b.score - a.score;
+    })
+
+    highScores.forEach(function(score) {
+        let l1 = document.createElement("li");
+        li.textContent = `${score.initials} - ${score.score}`
+
+        let ol = document.getElementById("highscores");
+        ol.appendChild(li);
+    })
 
 }
 
 function clearHighScores(){
-
+    localStorage.removeItem("highscore");
+    window.location.reload();
 }
 
 let clearButton = document.getElementById("clear");
 clearButton.addEventListener("click", clearHighScores);
 
 
-//document.getElementById("clear").onClick = clearHighScores;
+document.getElementById("clear").onClick = clearHighScores;
 
 printHighScores()
